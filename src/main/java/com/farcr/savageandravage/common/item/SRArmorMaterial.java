ed2fef449a821d79e836480c7ad033395daf5ac7
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-03-21 11:08:19 -0500

    Progress on Griefer armor

---
+++ src/main/java/com/farcr/savageandravage/common/item/SRArmorMaterial.java
@@ -0,0 +1,67 @@
+package com.farcr.savageandravage.common.item;
+
+import java.util.function.Supplier;
+import com.farcr.savageandravage.core.registry.SRItems;
+
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public enum SRArmorMaterial implements IArmorMaterial {
+	GRIEFER("griefer", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, () -> {
+	      return Ingredient.fromItems(SRItems.BLAST_PROOF_PLATING.get());
+	});
+	
+	private static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
+	private final String name;
+	private final int maxDamageFactor;
+	private final int[] damageReductionAmountArray;
+	private final int enchantability;
+	private final SoundEvent soundEvent;
+	private final float toughness;
+	private final LazyValue<Ingredient> repairMaterial;
+
+	private SRArmorMaterial(String nameIn, int maxDamageFactorIn, int[] damageReductionAmountsIn, int enchantabilityIn, SoundEvent equipSoundIn, float p_i48533_8_, Supplier<Ingredient> repairMaterialSupplier) {
+		this.name = nameIn;
+		this.maxDamageFactor = maxDamageFactorIn;
+		this.damageReductionAmountArray = damageReductionAmountsIn;
+		this.enchantability = enchantabilityIn;
+		this.soundEvent = equipSoundIn;
+		this.toughness = p_i48533_8_;
+		this.repairMaterial = new LazyValue<>(repairMaterialSupplier);
+	}
+
+	public int getDurability(EquipmentSlotType slotIn) {
+		return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+	}
+
+	public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+		return this.damageReductionAmountArray[slotIn.getIndex()];
+	}
+
+	public int getEnchantability() {
+		return this.enchantability;
+	}
+
+	public SoundEvent getSoundEvent() {
+		return this.soundEvent;
+	}
+
+	public Ingredient getRepairMaterial() {
+		return this.repairMaterial.getValue();
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public String getName() {
+		return this.name;
+	}
+
+	public float getToughness() {
+		return this.toughness;
+	}
+}
