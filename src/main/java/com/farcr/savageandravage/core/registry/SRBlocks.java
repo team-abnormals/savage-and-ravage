e09a0803dcfb15a9a1e84e19e8907f2f3f7e16aa
Farcr <40238086+Farcr@users.noreply.github.com>
2020-03-21 15:15:09 +0100

    completed gloomy tile implementation + a few placeholder items with no use yet

--- src/main/java/com/farcr/savageandravage/core/registry/SRBlocks.java
+++ src/main/java/com/farcr/savageandravage/core/registry/SRBlocks.java
@@ -15,5 +15,11 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class SRBlocks {
 	public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, SavageAndRavage.MODID);
 
-	public static RegistryObject<Block> GLOOMY_TILES = RegistryUtils.createBlock("gloomy_tiles", () -> new Block(Block.Properties.from(Blocks.PURPUR_BLOCK)), ItemGroup.BUILDING_BLOCKS);
+	public static RegistryObject<Block> GLOOMY_TILES                = RegistryUtils.createBlock("gloomy_tiles", () -> new Block(Block.Properties.from(Blocks.PURPUR_BLOCK)), ItemGroup.BUILDING_BLOCKS);
+	public static RegistryObject<Block> GLOOMY_TILE_STAIRS 		    = RegistryUtils.createBlock("gloomy_tile_stairs", () -> new StairsBlock(GLOOMY_TILES.get().getDefaultState(), BlockProperties.GLOOMY_TILES), ItemGroup.BUILDING_BLOCKS);
+	public static RegistryObject<Block> GLOOMY_TILE_SLAB 			= RegistryUtils.createBlock("gloomy_tile_slab", () -> new SlabBlock(BlockProperties.GLOOMY_TILES), ItemGroup.BUILDING_BLOCKS);
+	public static RegistryObject<Block> GLOOMY_TILE_WALL 			= RegistryUtils.createBlock("gloomy_tile_wall", () -> new WallBlock(BlockProperties.GLOOMY_TILES), ItemGroup.DECORATIONS);
+	public static RegistryObject<Block> GLOOMY_TILE_VERTICAL_SLAB   = RegistryUtils.createBlockCompat("quark", "gloomy_tile_vertical_slab", () -> new VerticalSlabBlock(BlockProperties.GLOOMY_TILES), ItemGroup.BUILDING_BLOCKS);
+	
+	public static RegistryObject<Block> CREEPER_SPORE_SACK		    = RegistryUtils.createBlockCompat("quark", "creeper_spore_sack", () -> new Block(Block.Properties.create(Material.WOOL, MaterialColor.LIME_TERRACOTTA).hardnessAndResistance(0.5F).sound(SoundType.CLOTH)), ItemGroup.DECORATIONS);
 }
--- src/main/java/com/farcr/savageandravage/core/registry/SRItems.java
+++ src/main/java/com/farcr/savageandravage/core/registry/SRItems.java
@@ -13,4 +13,10 @@ public class SRItems {
 	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, SavageAndRavage.MODID);
 	
 	public static RegistryObject<Item> BLAST_PROOF_PLATING = RegistryUtils.createItem("blast_proof_plating", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+	public static RegistryObject<Item> GRIEFER_HELMET = RegistryUtils.createItem("griefer_helmet", () -> new Item(new Item.Properties().group(ItemGroup.COMBAT)));
+	public static RegistryObject<Item> GRIEFER_CHESTPLATE = RegistryUtils.createItem("griefer_chestplate", () -> new Item(new Item.Properties().group(ItemGroup.COMBAT)));
+	public static RegistryObject<Item> GRIEFER_LEGGINGS = RegistryUtils.createItem("griefer_leggings", () -> new Item(new Item.Properties().group(ItemGroup.COMBAT)));
+	public static RegistryObject<Item> GRIEFER_BOOTS = RegistryUtils.createItem("griefer_boots", () -> new Item(new Item.Properties().group(ItemGroup.COMBAT)));
+	
+	public static RegistryObject<Item> CREEPER_SPORES = RegistryUtils.createItem("creeper_spores", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
 }
