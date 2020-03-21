3b3abea8168efbaa08b033ab244f0c25fa846506
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-03-21 09:44:23 -0500

    fixes and stuff

---
+++ src/main/java/com/farcr/savageandravage/core/util/BlockProperties.java
@@ -0,0 +1,10 @@
+package com.farcr.savageandravage.core.util;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+
+public class BlockProperties {
+
+	public static final Block.Properties GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.GRAY).hardnessAndResistance(1.5F, 6.0F);
+}
