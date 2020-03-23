d7a5429b419f5309d30f4d62688d53f2e3dded05
Voliant <thatvaliantvole@gmail.com>
2020-03-23 22:23:57 +0000

    cringe ass nae nae grown up creepie what will be do

--- src/main/java/com/farcr/savageandravage/client/render/CreepieRenderer.java
+++ src/main/java/com/farcr/savageandravage/client/render/CreepieRenderer.java
@@ -2,18 +2,23 @@ package com.farcr.savageandravage.client.render;
 
 import com.farcr.savageandravage.client.model.CreepieModel;
 import com.farcr.savageandravage.common.entity.CreepieEntity;
+import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.model.CreeperModel;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
-public class CreepieRenderer extends MobRenderer<CreepieEntity, CreepieModel<CreepieEntity>>{
+public class CreepieRenderer extends MobRenderer<CreepieEntity, CreeperModel<CreepieEntity>>{
     //private static final ResourceLocation CREEPIE_TEXTURES = new ResourceLocation("savageandravage:textures/entity/creepie.png");
     private static final ResourceLocation CREEPIE_TEXTURES = new ResourceLocation("textures/entity/creeper/creeper.png");
 
     public CreepieRenderer(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn, new CreepieModel(), 0.3F);
+        super(renderManagerIn, new CreeperModel<CreepieEntity>(), 0.3F);
+    }
+
+    protected void preRenderCallback(CreepieEntity entitylivingbaseIn, float partialTickTime){
+        GlStateManager.scalef(-5.0F, -5.0F, -5.0F);
     }
 
     @Override
