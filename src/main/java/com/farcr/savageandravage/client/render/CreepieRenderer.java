c8c2faa464a52649718d65d17b9d4605b319d1b1
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-03-23 15:46:44 -0700

    it is fixed

--- src/main/java/com/farcr/savageandravage/client/render/CreepieRenderer.java
+++ src/main/java/com/farcr/savageandravage/client/render/CreepieRenderer.java
@@ -1,25 +1,25 @@
 package com.farcr.savageandravage.client.render;
 
-import com.farcr.savageandravage.client.model.CreepieModel;
 import com.farcr.savageandravage.common.entity.CreepieEntity;
-import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.matrix.MatrixStack;
+
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.model.CreeperModel;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.GiantEntity;
 import net.minecraft.util.ResourceLocation;
 
 public class CreepieRenderer extends MobRenderer<CreepieEntity, CreeperModel<CreepieEntity>>{
     //private static final ResourceLocation CREEPIE_TEXTURES = new ResourceLocation("savageandravage:textures/entity/creepie.png");
     private static final ResourceLocation CREEPIE_TEXTURES = new ResourceLocation("textures/entity/creeper/creeper.png");
-
+    
     public CreepieRenderer(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn, new CreeperModel<CreepieEntity>(), 0.3F);
+		super(renderManagerIn, new CreeperModel<CreepieEntity>(), 0.3F);
     }
 
-    protected void preRenderCallback(CreepieEntity entitylivingbaseIn, float partialTickTime){
-        GlStateManager.scalef(-5.0F, -5.0F, -5.0F);
-    }
+    protected void preRenderCallback(CreepieEntity entitylivingbaseIn, MatrixStack matrixStackIn, float partialTickTime) {
+        matrixStackIn.scale(0.5F, 0.5F, 0.5F);
+     }
 
     @Override
     public ResourceLocation getEntityTexture(CreepieEntity entity) {
