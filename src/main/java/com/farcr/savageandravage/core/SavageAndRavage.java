75df3508651c67f5462ebd8bf3e9433fe7fb39ec
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-04-02 15:52:56 -0700

    fixed spawn egg color issue

--- src/main/java/com/farcr/savageandravage/core/SavageAndRavage.java
+++ src/main/java/com/farcr/savageandravage/core/SavageAndRavage.java
@@ -1,16 +1,22 @@
 package com.farcr.savageandravage.core;
 
+import com.farcr.savageandravage.common.item.SRSpawnEgg;
 import com.farcr.savageandravage.core.events.SREvents;
 import com.farcr.savageandravage.core.registry.SRBlocks;
 import com.farcr.savageandravage.core.registry.SREntities;
 import com.farcr.savageandravage.core.registry.SRItems;
 import com.farcr.savageandravage.core.registry.SRParticles;
 
+import net.minecraft.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -36,8 +42,29 @@ public class SavageAndRavage
         modEventBus.addListener(this::commonSetup);
     	DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
         	modEventBus.addListener(this::clientSetup);
+    		modEventBus.addListener(EventPriority.LOWEST, this::registerItemColors);
         });
     }
+    
+    /**
+     * @author SmellyModder(Luke Tonon)
+     */
+	private void registerItemColors(ColorHandlerEvent.Item event) 
+	{
+	   for(RegistryObject<Item> items : SRItems.SPAWN_EGGS) 
+	   {
+	    if (ObfuscationReflectionHelper.getPrivateValue(RegistryObject.class, items, "value") != null) {
+	    Item item = items.get();
+	    if (item instanceof SRSpawnEgg) 
+	    {
+	       event.getItemColors().register((itemColor, itemsIn) -> 
+	       {
+	         return ((SRSpawnEgg) item).getColor(itemsIn);
+	       }, item);
+	    }
+	  }
+	 }
+	}
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         //SRData.registerBlockData();
