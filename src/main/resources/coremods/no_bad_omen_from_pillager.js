933e9af83fc069225e3f4ab875116f978d35f21f
Voliant <thatvaliantvole@gmail.com>
2020-04-29 08:49:55 +0100

    Remade coremod to be more stable (hopefully)

--- src/main/resources/coremods/no_bad_omen_from_pillager.js
+++ src/main/resources/coremods/no_bad_omen_from_pillager.js
@@ -1,3 +1,5 @@
+var Opcodes = org.objectweb.asm.Opcodes;
+
 function initializeCoreMod() {
     return {
         'no-bad-omen-from-pillager': {
@@ -8,11 +10,23 @@ function initializeCoreMod() {
                 'methodDesc': '(Lnet/minecraft/util/DamageSource;)V'
             },
             'transformer': function(method) {
+                print("[SavageAndRavage/COREMOD] Patching onDeath() in AbstractRaiderEntity");
                 for (var i = 0; i < method.instructions.size(); ++i) {
-                    var instruction = method.instructions.get(i);
-                	if((i>=154&&i<=155)||(i>=191&&i<=192)){ //these are the specific instructions I want to remove
-                        method.instructions.remove(instruction);
-                        print("[SavageAndRavage/COREMOD] Successfully removed instruction "+instruction+" in AbstractRaiderEntity, whose instruction number in onDeath() was "+i);
+                	var instruction = method.instructions.get(i);
+                    if(instruction.getOpcode()==Opcodes.INVOKEVIRTUAL && instruction.getNext().getOpcode()==Opcodes.POP){
+                            var instruction2 = instruction.getPrevious();
+                            var instruction1 = instruction2.getPrevious();
+                            var instruction3 = instruction.getNext();
+                            method.instructions.remove(instruction1);
+                            print("[SavageAndRavage/COREMOD] Removed instruction"+instruction1);
+                            method.instructions.remove(instruction2);
+                            print("[SavageAndRavage/COREMOD] Removed instruction"+instruction2);
+                            method.instructions.remove(instruction);
+                            print("[SavageAndRavage/COREMOD] Removed instruction"+instruction);
+                            method.instructions.remove(instruction3);
+                            print("[SavageAndRavage/COREMOD] Removed instruction"+instruction3);
+                            //if(instruction.getPrevious().getOpcode()==Opcodes.GETSTATIC){
+                            //else if((instruction.getPrevious().getOpcode()==Opcodes.ALOAD)){
                     }
                 }
                 return method;
