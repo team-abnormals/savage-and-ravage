ebd113034b2bebf4448c9541baff115ce96f2715
Voliant <thatvaliantvole@gmail.com>
2020-05-17 23:49:57 +0100

    Changed baby effect and potion names, added effect icon
    Effect is Shrinking, potion is Youth

--- src/main/java/com/farcr/savageandravage/core/registry/SREffects.java
+++ src/main/java/com/farcr/savageandravage/core/registry/SREffects.java
@@ -16,23 +16,23 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class SREffects {
     public static final DeferredRegister<Effect> EFFECTS = new DeferredRegister<>(ForgeRegistries.POTIONS, SavageAndRavage.MODID);
-    //ForgeRegistries.POTIONS is quite badly named, MCP moment.
+    //ForgeRegistries.POTIONS is badly named, MCP moment.
     public static final DeferredRegister<Potion> POTIONS = new DeferredRegister<>(ForgeRegistries.POTION_TYPES, SavageAndRavage.MODID);
     //Same for this, potion types???
 
     public static final RegistryObject<Effect> GROWING = EFFECTS.register("growth", EffectGrowth::new);
-    public static final RegistryObject<Effect> BABY = EFFECTS.register("baby", EffectBaby::new);
+    public static final RegistryObject<Effect> SHRINKING = EFFECTS.register("shrinking", EffectBaby::new);
 
     public static final RegistryObject<Potion> GROWING_NORMAL  = POTIONS.register("growing", () -> new Potion(new EffectInstance(GROWING.get(), 600)));
     public static final RegistryObject<Potion> GROWING_LONG   = POTIONS.register("growing_long", () -> new Potion(new EffectInstance(GROWING.get(), 1800)));
-    public static final RegistryObject<Potion> BABY_NORMAL  = POTIONS.register("baby", () -> new Potion(new EffectInstance(BABY.get(), 600)));
-    public static final RegistryObject<Potion> BABY_LONG   = POTIONS.register("baby_long", () -> new Potion(new EffectInstance(BABY.get(), 1800)));
+    public static final RegistryObject<Potion> YOUTH_NORMAL  = POTIONS.register("youth", () -> new Potion(new EffectInstance(SHRINKING.get(), 600)));
+    public static final RegistryObject<Potion> YOUTH_LONG   = POTIONS.register("youth_long", () -> new Potion(new EffectInstance(SHRINKING.get(), 1800)));
 
     public static void registerBrewingRecipes(){
         PotionBrewing.addMix(Potions.AWKWARD, Items.GOLDEN_APPLE, GROWING_NORMAL.get());
         PotionBrewing.addMix(GROWING_NORMAL.get(), Items.REDSTONE, GROWING_LONG.get());
-        PotionBrewing.addMix(GROWING_NORMAL.get(), Items.FERMENTED_SPIDER_EYE, BABY_NORMAL.get());
-        PotionBrewing.addMix(GROWING_LONG.get(), Items.FERMENTED_SPIDER_EYE, BABY_LONG.get());
-        PotionBrewing.addMix(BABY_NORMAL.get(), Items.REDSTONE, BABY_LONG.get());
+        PotionBrewing.addMix(GROWING_NORMAL.get(), Items.FERMENTED_SPIDER_EYE, YOUTH_NORMAL.get());
+        PotionBrewing.addMix(GROWING_LONG.get(), Items.FERMENTED_SPIDER_EYE, YOUTH_LONG.get());
+        PotionBrewing.addMix(YOUTH_NORMAL.get(), Items.REDSTONE, YOUTH_LONG.get());
     }
 }
