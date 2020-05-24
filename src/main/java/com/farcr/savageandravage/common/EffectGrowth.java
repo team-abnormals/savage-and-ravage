a49bb948de1a94d19ec7d5b1646056e5d7181950
Voliant <thatvaliantvole@gmail.com>
2020-05-24 14:03:20 +0100

    Fixed parrot edge case for potions

--- src/main/java/com/farcr/savageandravage/common/EffectGrowth.java
+++ src/main/java/com/farcr/savageandravage/common/EffectGrowth.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.SlimeEntity;
 import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.entity.passive.ParrotEntity;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
@@ -21,9 +22,9 @@ public class EffectGrowth extends Effect {
     public void performEffect(LivingEntity entity, int amplifier) {
         boolean canGrow = false;
         if(entity instanceof SlimeEntity && ((SlimeEntity)entity).getSlimeSize()<3) canGrow = true;
-        else if (SREvents.checkBooflo(entity,false)) canGrow = true;
+        else if(SREvents.checkBooflo(entity,false)) canGrow = true;
         else if(entity.isChild()){
-            if(entity instanceof AgeableEntity || entity instanceof CreepieEntity || entity instanceof ZombieEntity) canGrow = true;
+            if((entity instanceof AgeableEntity && !(entity instanceof ParrotEntity)) || entity instanceof CreepieEntity || entity instanceof ZombieEntity) canGrow = true;
         }
         if(canGrow && entity.getRNG().nextInt(3)==0){
             if(entity.isServerWorld()) ((ServerWorld) entity.world).spawnParticle(ParticleTypes.HAPPY_VILLAGER, entity.getPosXRandom(0.3D), entity.getPosYRandom(), entity.getPosZRandom(0.3D), 1, 0.3D, 0.3D, 0.3D, 1.0D);
--- src/main/java/com/farcr/savageandravage/common/EffectShrinking.java
+++ src/main/java/com/farcr/savageandravage/common/EffectShrinking.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.entity.monster.SlimeEntity;
 import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.entity.passive.ParrotEntity;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
@@ -21,9 +22,9 @@ public class EffectShrinking extends Effect {
     public void performEffect(LivingEntity entity, int amplifier) {
         boolean canBaby = false;
         if(entity instanceof SlimeEntity && ((SlimeEntity)entity).getSlimeSize()>1) canBaby = true;
-        else if (SREvents.checkBooflo(entity,true)) canBaby = true;
-        else if (!entity.isChild()) {
-            if (entity instanceof AgeableEntity || entity instanceof CreeperEntity || entity instanceof ZombieEntity) canBaby = true;
+        else if(SREvents.checkBooflo(entity,true)) canBaby = true;
+        else if(!entity.isChild()) {
+            if((entity instanceof AgeableEntity && !(entity instanceof ParrotEntity)) || entity instanceof CreeperEntity || entity instanceof ZombieEntity) canBaby = true;
         }
         if(canBaby && entity.getRNG().nextInt(3)==0){
             if(entity.isServerWorld()) ((ServerWorld) entity.world).spawnParticle(ParticleTypes.TOTEM_OF_UNDYING, entity.getPosXRandom(0.3D), entity.getPosYRandom(), entity.getPosZRandom(0.3D), 1, 0.3D, 0.3D, 0.3D, 0.2D);
