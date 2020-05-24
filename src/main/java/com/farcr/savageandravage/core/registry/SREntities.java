b60f17d4475297beebff2a6b566b4726804149ab
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-05-24 14:19:40 -0700

    made skeleton villagers spawn in stone shore biomes

--- src/main/java/com/farcr/savageandravage/core/registry/SREntities.java
+++ src/main/java/com/farcr/savageandravage/core/registry/SREntities.java
@@ -19,6 +19,7 @@ import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -56,7 +57,7 @@ public class SREntities {
 	}
 
     public static void addSpawns(Biome biome) {
-    	if (biome.getCategory() != Biome.Category.NETHER && biome.getCategory() != Biome.Category.THEEND && biome.getCategory() != Biome.Category.MUSHROOM && biome.getCategory() != Biome.Category.NONE) {
+    	if (biome.getCategory() != Biome.Category.NETHER && biome.getCategory() != Biome.Category.THEEND && biome.getCategory() != Biome.Category.MUSHROOM && biome != Biomes.THE_VOID) {
     		biome.addSpawn(EntityClassification.MONSTER, new Biome.SpawnListEntry(SKELETON_VILLAGER.get(), 5, 5, 5)); //Rationalisation for this is that it used to be a pillager patrol
     	}
     }
