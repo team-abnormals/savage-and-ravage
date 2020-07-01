3d3d18ff03f9fb9b798484c456af9a0d68753bb2
Voliant <thatvaliantvole@gmail.com>
2020-07-01 21:15:12 +0100

    wait im a stupid dumb dumb lmao

--- src/main/java/com/farcr/savageandravage/common/entity/goals/ImprovedCrossbowGoal.java
+++ src/main/java/com/farcr/savageandravage/common/entity/goals/ImprovedCrossbowGoal.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.ICrossbowUser;
 import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.merchant.villager.AbstractVillagerEntity;
 import net.minecraft.entity.monster.PillagerEntity;
