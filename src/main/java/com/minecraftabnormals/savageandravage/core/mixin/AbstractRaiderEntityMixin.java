36073abb6da77c233dab9dea8256af316036ae6f
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-08-11 12:09:48 -0500

    Reminder to convert coremod to Mixin

---
+++ src/main/java/com/minecraftabnormals/savageandravage/core/mixin/AbstractRaiderEntityMixin.java
@@ -0,0 +1,10 @@
+package com.minecraftabnormals.savageandravage.core.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.entity.monster.AbstractRaiderEntity;
+
+@Mixin(AbstractRaiderEntity.class)
+public class AbstractRaiderEntityMixin {
+    //TODO: Convert no_bad_omen_from_pillager.js to AbstractRaiderEntityMixin.java
+}
