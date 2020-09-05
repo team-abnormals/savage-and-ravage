30ebba05957c09f7fbd475d78834a11a499a7e25
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-09-05 09:48:26 -0700

    reformatted spore bomb block class

--- src/main/java/com/minecraftabnormals/savageandravage/common/block/SporeBombBlock.java
+++ src/main/java/com/minecraftabnormals/savageandravage/common/block/SporeBombBlock.java
@@ -20,45 +20,42 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 
-public class SporeBombBlock extends TNTBlock
-{
-	   public SporeBombBlock(Block.Properties properties) {
-	      super(properties);
-	   }
-	   @Override
-	   public void catchFire(BlockState state, World world, BlockPos pos, @Nullable net.minecraft.util.Direction face, @Nullable LivingEntity igniter) {
-		 SporeBombEntity sporebomb = new SporeBombEntity(world, (double)pos.getX() + 0.5D, (double)pos.getY(), (double)pos.getZ() + 0.5D, igniter);
-		 world.addEntity(sporebomb);
-		 world.playSound((PlayerEntity)null, sporebomb.getPosX(), sporebomb.getPosY(), sporebomb.getPosZ(), SoundEvents.ENTITY_CREEPER_PRIMED, SoundCategory.BLOCKS, 1.0F, 0.5F);
-	   }
-	   
-	   @Override
-	   public void onExplosionDestroy(World worldIn, BlockPos pos, Explosion explosionIn) 
-	   {
-	     SporeBombEntity sporebomb = new SporeBombEntity(worldIn, (double)((float)pos.getX() + 0.5F), (double)pos.getY(), (double)((float)pos.getZ() + 0.5F), explosionIn.getExplosivePlacedBy());
-	     sporebomb.setFuse((short)(worldIn.rand.nextInt(sporebomb.getFuse() / 4) + sporebomb.getFuse() / 8));
-	     worldIn.addEntity(sporebomb);
-	   }
-	   
-	   @Override
-	   public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) 
-	   {
-		  if (worldIn.isBlockPowered(pos)) 
-		  {
-		    this.catchFire(state, worldIn, pos, null, null);
-		    worldIn.removeBlock(pos, false);
-		  }
+public class SporeBombBlock extends TNTBlock {
+    public SporeBombBlock(Block.Properties properties) {
+        super(properties);
     }
-	   
-	   @Override
-		public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
-			if(ItemStackUtils.isInGroup(this.asItem(), group)) {
-				int targetIndex = ItemStackUtils.findIndexOfItem(Items.TNT, items);
-				if(targetIndex != -1) {
-					items.add(targetIndex + 1, new ItemStack(this));
-				} else {
-					super.fillItemGroup(group, items);
-				}
-			}
-		}
+
+    @Override
+    public void catchFire(BlockState state, World world, BlockPos pos, @Nullable net.minecraft.util.Direction face, @Nullable LivingEntity igniter) {
+        SporeBombEntity sporebomb = new SporeBombEntity(world, (double) pos.getX() + 0.5D, (double) pos.getY(), (double) pos.getZ() + 0.5D, igniter);
+        world.addEntity(sporebomb);
+        world.playSound((PlayerEntity) null, sporebomb.getPosX(), sporebomb.getPosY(), sporebomb.getPosZ(), SoundEvents.ENTITY_CREEPER_PRIMED, SoundCategory.BLOCKS, 1.0F, 0.5F);
+    }
+
+    @Override
+    public void onExplosionDestroy(World worldIn, BlockPos pos, Explosion explosionIn) {
+        SporeBombEntity sporebomb = new SporeBombEntity(worldIn, (double) ((float) pos.getX() + 0.5F), (double) pos.getY(), (double) ((float) pos.getZ() + 0.5F), explosionIn.getExplosivePlacedBy());
+        sporebomb.setFuse((short) (worldIn.rand.nextInt(sporebomb.getFuse() / 4) + sporebomb.getFuse() / 8));
+        worldIn.addEntity(sporebomb);
+    }
+
+    @Override
+    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+        if (worldIn.isBlockPowered(pos)) {
+            this.catchFire(state, worldIn, pos, null, null);
+            worldIn.removeBlock(pos, false);
+        }
+    }
+
+    @Override
+    public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
+        if (ItemStackUtils.isInGroup(this.asItem(), group)) {
+            int targetIndex = ItemStackUtils.findIndexOfItem(Items.TNT, items);
+            if (targetIndex != -1) {
+                items.add(targetIndex + 1, new ItemStack(this));
+            } else {
+                super.fillItemGroup(group, items);
+            }
+        }
+    }
 }
