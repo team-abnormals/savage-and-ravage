ede0b6b19959a3a0c30a1c94eca48a64b277e2d4
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-09-26 15:49:00 -0700

    made iron golems and zoglins attacking creepers configurable

--- src/main/java/com/minecraftabnormals/savageandravage/core/mixin/ZoglinEntityMixin.java
+++ src/main/java/com/minecraftabnormals/savageandravage/core/mixin/ZoglinEntityMixin.java
@@ -5,6 +5,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.minecraftabnormals.savageandravage.core.SRConfig;
+
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.ZoglinEntity;
@@ -15,6 +17,6 @@ public class ZoglinEntityMixin {
 
     @Inject(at = @At("RETURN"), method = "func_234337_j_(Lnet/minecraft/entity/LivingEntity;)Z", cancellable = true)
     private static boolean func_234337_j_(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
-        return entity.getType() != EntityType.ZOGLIN && EntityPredicates.field_233583_f_.test(entity);
+        return SRConfig.COMMON.creeperExplosionsDestroyBlocks.get() ? entity.getType() != EntityType.ZOGLIN && entity.getType() != EntityType.CREEPER && EntityPredicates.field_233583_f_.test(entity) : entity.getType() != EntityType.ZOGLIN && EntityPredicates.field_233583_f_.test(entity) ;
     }
 }
