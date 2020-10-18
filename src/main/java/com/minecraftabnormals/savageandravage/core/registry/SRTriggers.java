94287d467736e38a2846c43739e68ac2b54b7571
Voliant <thatvaliantvole@gmail.com>
2020-10-18 23:14:16 +0100

    Removed redundant @author
    Seems to be a holdover from when SRTriggers was EmptyTrigger, which was added into AC.

--- src/main/java/com/minecraftabnormals/savageandravage/core/registry/SRTriggers.java
+++ src/main/java/com/minecraftabnormals/savageandravage/core/registry/SRTriggers.java
@@ -7,9 +7,6 @@ import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 
-/**
- * @author - SmellyModder(Luke Tonon)
- */
 @Mod.EventBusSubscriber(modid = SavageAndRavage.MODID)
 public class SRTriggers {
     public static final EmptyTrigger BURN_BANNER = CriteriaTriggers.register(new EmptyTrigger(prefix("burn_banner")));
