3c901bf51b8347ff41ad598753e206e95b4dd9b7
abigailfails <thatvaliantvole@gmail.com>
2020-11-18 16:33:41 +0000

    Made gloomy tiles more like stone

--- src/main/java/com/minecraftabnormals/savageandravage/core/other/SRProperties.java
+++ src/main/java/com/minecraftabnormals/savageandravage/core/other/SRProperties.java
@@ -10,9 +10,9 @@ import java.util.function.ToIntFunction;
 
 public class SRProperties {
 
-    public static final Block.Properties GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.GRAY).hardnessAndResistance(1.5F, 6.0F);
-    public static final Block.Properties LIGHTABLE_GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.GRAY).hardnessAndResistance(1.5F, 6.0F).setLightLevel(getLightValuePowered(7));
-    public static final Block.Properties BLAST_PROOF_PLATES = Block.Properties.create(Material.IRON, MaterialColor.GREEN).hardnessAndResistance(2.0F, 1200.0F);
+    public static final Block.Properties GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.GRAY).setRequiresTool().hardnessAndResistance(1.5F, 6.0F);
+    public static final Block.Properties LIGHTABLE_GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.GRAY).setRequiresTool().hardnessAndResistance(1.5F, 6.0F).setLightLevel(getLightValuePowered(7));
+    public static final Block.Properties BLAST_PROOF_PLATES = Block.Properties.create(Material.IRON, MaterialColor.GREEN).setRequiresTool().hardnessAndResistance(2.0F, 1200.0F);
 
     public static ToIntFunction<BlockState> getLightValuePowered(int lightValue) {
         return (stateHolder) -> stateHolder.get(BlockStateProperties.POWERED) ? lightValue : 0;
