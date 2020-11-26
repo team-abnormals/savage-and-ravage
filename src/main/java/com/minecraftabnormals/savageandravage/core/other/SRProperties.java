e930f5a28c90d2ace14423ad98668119679c1152
abigailfails <thatvaliantvole@gmail.com>
2020-11-26 19:52:26 +0000

    Updated gloomy tile properties

--- src/main/java/com/minecraftabnormals/savageandravage/core/other/SRProperties.java
+++ src/main/java/com/minecraftabnormals/savageandravage/core/other/SRProperties.java
@@ -10,8 +10,8 @@ import java.util.function.ToIntFunction;
 
 public class SRProperties {
 
-    public static final Block.Properties GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.GRAY).setRequiresTool().hardnessAndResistance(1.5F, 6.0F);
-    public static final Block.Properties LIGHTABLE_GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.GRAY).setRequiresTool().hardnessAndResistance(1.5F, 6.0F).setLightLevel(getLightValuePowered(7));
+    public static final Block.Properties GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.LIGHT_BLUE_TERRACOTTA).setRequiresTool().hardnessAndResistance(1.5F, 6.0F);
+    public static final Block.Properties LIGHTABLE_GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.LIGHT_BLUE_TERRACOTTA).setRequiresTool().hardnessAndResistance(1.5F, 6.0F).setLightLevel(getLightValuePowered(7));
     public static final Block.Properties BLAST_PROOF_PLATES = Block.Properties.create(Material.IRON, MaterialColor.GREEN).setRequiresTool().hardnessAndResistance(2.0F, 1200.0F);
 
     public static ToIntFunction<BlockState> getLightValuePowered(int lightValue) {
