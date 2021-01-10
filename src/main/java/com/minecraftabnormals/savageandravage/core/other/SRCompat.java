c9b5b831a8bf53b256b74019b2bf23f8ab6c82f4
abigailfails <thatvaliantvole@gmail.com>
2021-01-10 14:22:22 +0000

    Used getEntitiesAtOffsetPos method for banner dispenser behavior

--- src/main/java/com/minecraftabnormals/savageandravage/core/other/SRCompat.java
+++ src/main/java/com/minecraftabnormals/savageandravage/core/other/SRCompat.java
@@ -22,12 +22,14 @@ import net.minecraft.item.Items;
 import net.minecraft.util.EntityPredicates;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Map;
+
+import static com.minecraftabnormals.abnormals_core.core.util.BlockUtil.getEntitiesAtOffsetPos;
+import static com.minecraftabnormals.abnormals_core.core.util.BlockUtil.offsetPos;
 
 public class SRCompat {
 
@@ -53,7 +55,7 @@ public class SRCompat {
 		DispenserBlock.registerDispenseBehavior(SRBlocks.SPORE_BOMB.get(), new DefaultDispenseItemBehavior() {
 			protected ItemStack dispenseStack(IBlockSource source, ItemStack stack) {
 				World world = source.getWorld();
-				BlockPos blockpos = source.getBlockPos().offset(source.getBlockState().get(DispenserBlock.FACING));
+				BlockPos blockpos = offsetPos(source);
 				SporeBombEntity sporeBomb = new SporeBombEntity(world, (double) blockpos.getX() + 0.5D, blockpos.getY(), (double) blockpos.getZ() + 0.5D, null);
 				world.addEntity(sporeBomb);
 				world.playSound(null, sporeBomb.getPosX(), sporeBomb.getPosY(), sporeBomb.getPosZ(), SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
@@ -61,12 +63,12 @@ public class SRCompat {
 				return stack;
 			}
 		});
-		ForgeRegistries.ITEMS.getEntries().stream().map(Map.Entry::getValue).filter(i -> i instanceof BannerItem).forEach(i -> DataUtil.registerAlternativeDispenseBehavior(i, (source, stack) -> !source.getWorld().getEntitiesWithinAABB(LivingEntity.class, new AxisAlignedBB(BlockUtil.offsetPos(source)), EntityPredicates.NOT_SPECTATING.and(new EntityPredicates.ArmoredMob(stack))).isEmpty(), ArmorItem.DISPENSER_BEHAVIOR));
-		DataUtil.registerAlternativeDispenseBehavior(Items.FLINT_AND_STEEL, (source, stack) -> SREvents.isValidBurningBannerPos(source.getWorld(), BlockUtil.offsetPos(source)), new DefaultDispenseItemBehavior() {
+		ForgeRegistries.ITEMS.getEntries().stream().map(Map.Entry::getValue).filter(i -> i instanceof BannerItem).forEach(i -> DataUtil.registerAlternativeDispenseBehavior(i, (source, stack) -> !getEntitiesAtOffsetPos(source, LivingEntity.class, EntityPredicates.NOT_SPECTATING.and(new EntityPredicates.ArmoredMob(stack))).isEmpty(), ArmorItem.DISPENSER_BEHAVIOR));
+		DataUtil.registerAlternativeDispenseBehavior(Items.FLINT_AND_STEEL, (source, stack) -> SREvents.isValidBurningBannerPos(source.getWorld(), offsetPos(source)), new DefaultDispenseItemBehavior() {
 			@Override
 			protected ItemStack dispenseStack(IBlockSource source, ItemStack stack) {
 				World world = source.getWorld();
-				world.addEntity(new BurningBannerEntity(world, BlockUtil.offsetPos(source), null));
+				world.addEntity(new BurningBannerEntity(world, offsetPos(source), null));
 				if (stack.attemptDamageItem(1, world.rand, null)) {
 					stack.setCount(0);
 				}
