fb3df46f860aa79eb9020df3797d39b63f626668
abigailfails <thatvaliantvole@gmail.com>
2021-01-15 17:35:07 +0000

    Blast proof plates have metal sounds

--- src/main/java/com/minecraftabnormals/savageandravage/core/registry/SRBlocks.java
+++ src/main/java/com/minecraftabnormals/savageandravage/core/registry/SRBlocks.java
@@ -43,7 +43,7 @@ public class SRBlocks {
 	public static class Properties {
 		public static final Block.Properties GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.LIGHT_BLUE_TERRACOTTA).setRequiresTool().hardnessAndResistance(1.5F, 6.0F);
 		public static final Block.Properties LIGHTABLE_GLOOMY_TILES = Block.Properties.create(Material.ROCK, MaterialColor.LIGHT_BLUE_TERRACOTTA).setRequiresTool().hardnessAndResistance(1.5F, 6.0F).setLightLevel(getLightValuePowered(7));
-		public static final Block.Properties BLAST_PROOF_PLATES = Block.Properties.create(Material.IRON, MaterialColor.GREEN).setRequiresTool().hardnessAndResistance(2.0F, 1200.0F);
+		public static final Block.Properties BLAST_PROOF_PLATES = Block.Properties.create(Material.IRON, MaterialColor.GREEN).setRequiresTool().hardnessAndResistance(2.0F, 1200.0F).sound(SoundType.METAL);
 
 		public static ToIntFunction<BlockState> getLightValuePowered(int lightValue) {
 			return (stateHolder) -> stateHolder.get(BlockStateProperties.POWERED) ? lightValue : 0;
