cbc1e740f02b3f49a99f25655a86623d99e6c004
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-01-21 20:52:02 -0600

    change loot table location

--- src/main/java/com/minecraftabnormals/savageandravage/core/other/SRLootTables.java
+++ src/main/java/com/minecraftabnormals/savageandravage/core/other/SRLootTables.java
@@ -1,9 +1,8 @@
 package com.minecraftabnormals.savageandravage.core.other;
 
 import com.minecraftabnormals.savageandravage.core.SavageAndRavage;
-
 import net.minecraft.util.ResourceLocation;
 
 public class SRLootTables {
-	public static final ResourceLocation CREEPER_EXPLOSION_DROPS = new ResourceLocation(SavageAndRavage.MOD_ID, "entities/creeper_explosion_drops");
+	public static final ResourceLocation CREEPER_EXPLOSION_DROPS = new ResourceLocation(SavageAndRavage.MOD_ID, "gameplay/creeper_explosion_drops");
 }
