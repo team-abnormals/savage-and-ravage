f83b55272f959a99bccc8ab1ad71a4227e4b65e5
abigailfails <thatvaliantvole@gmail.com>
2021-03-14 12:26:02 +0000

    more wip trickster

--- src/main/java/com/minecraftabnormals/savageandravage/common/entity/TricksterEntity.java
+++ src/main/java/com/minecraftabnormals/savageandravage/common/entity/TricksterEntity.java
@@ -3,14 +3,27 @@ package com.minecraftabnormals.savageandravage.common.entity;
 import com.minecraftabnormals.savageandravage.core.registry.SRItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.AvoidEntityGoal;
+import net.minecraft.entity.ai.goal.HurtByTargetGoal;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.RandomWalkingGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.merchant.villager.AbstractVillagerEntity;
+import net.minecraft.entity.monster.AbstractRaiderEntity;
 import net.minecraft.entity.monster.SpellcastingIllagerEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class TricksterEntity extends SpellcastingIllagerEntity {
     public TricksterEntity(EntityType<? extends SpellcastingIllagerEntity> type, World p_i48551_2_) {
@@ -20,6 +33,18 @@ public class TricksterEntity extends SpellcastingIllagerEntity {
     @Override
     protected void registerGoals() {
         super.registerGoals();
+        this.goalSelector.addGoal(0, new SwimGoal(this));
+        this.goalSelector.addGoal(1, new CastingASpellGoal());
+        this.goalSelector.addGoal(2, new AvoidEntityGoal<>(this, PlayerEntity.class, 8.0F, 0.6D, 1.0D));
+        this.goalSelector.addGoal(5, new CreatePrisonGoal());
+        this.goalSelector.addGoal(6, new ThrowSparkGoal());
+        this.goalSelector.addGoal(8, new RandomWalkingGoal(this, 0.6D));
+        this.goalSelector.addGoal(9, new LookAtGoal(this, PlayerEntity.class, 3.0F, 1.0F));
+        this.goalSelector.addGoal(10, new LookAtGoal(this, MobEntity.class, 8.0F));
+        this.targetSelector.addGoal(1, (new HurtByTargetGoal(this, AbstractRaiderEntity.class)).setCallsForHelp());
+        this.targetSelector.addGoal(2, (new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true)).setUnseenMemoryTicks(300));
+        this.targetSelector.addGoal(3, (new NearestAttackableTargetGoal<>(this, AbstractVillagerEntity.class, false)).setUnseenMemoryTicks(300));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolemEntity.class, false));
     }
 
     @Override
@@ -43,5 +68,63 @@ public class TricksterEntity extends SpellcastingIllagerEntity {
 
     public static AttributeModifierMap.MutableAttribute registerAttributes() {
         return LivingEntity.registerAttributes().createMutableAttribute(Attributes.FOLLOW_RANGE, 16.0D);
+    }
+
+    class CreatePrisonGoal extends UseSpellGoal {
+
+        @Override
+        protected void castSpell() {
+            //TODO set ticking particle task then prison
+        }
+
+        @Override
+        protected int getCastingTime() {
+            return 0;
+        }
+
+        @Override
+        protected int getCastingInterval() {
+            return 0;
+        }
+
+        @Nullable
+        @Override
+        protected SoundEvent getSpellPrepareSound() {
+            return null;
+        }
+
+        @Override
+        protected SpellType getSpellType() {
+            return null;
+        }
+    }
+
+    class ThrowSparkGoal extends UseSpellGoal {
+
+        @Override
+        protected void castSpell() {
+
+        }
+
+        @Override
+        protected int getCastingTime() {
+            return 0;
+        }
+
+        @Override
+        protected int getCastingInterval() {
+            return 0;
+        }
+
+        @Nullable
+        @Override
+        protected SoundEvent getSpellPrepareSound() {
+            return null;
+        }
+
+        @Override
+        protected SpellType getSpellType() {
+            return null;
+        }
     }
 }
