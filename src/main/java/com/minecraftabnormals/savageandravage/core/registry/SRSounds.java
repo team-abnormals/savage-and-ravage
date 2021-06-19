b717ba04adf9fa8f77c911308ec3c5eb08779f44
abigailfails <thatvaliantvole@gmail.com>
2021-06-19 21:07:15 +0100

    Added trickster sounds

--- src/main/java/com/minecraftabnormals/savageandravage/core/registry/SRSounds.java
+++ src/main/java/com/minecraftabnormals/savageandravage/core/registry/SRSounds.java
@@ -15,11 +15,20 @@ public class SRSounds {
 	public static final RegistryObject<SoundEvent> ENTITY_CREEPIE_PRIMED = HELPER.createSoundEvent("entity.creepie.primed");
 	public static final RegistryObject<SoundEvent> ENTITY_CREEPIE_CONVERT = HELPER.createSoundEvent("entity.creepie.convert");
 	public static final RegistryObject<SoundEvent> ENTITY_CREEPIE_GROW = HELPER.createSoundEvent("entity.creepie.grow");
+
 	public static final RegistryObject<SoundEvent> ENTITY_ICEOLOGER_AMBIENT = HELPER.createSoundEvent("entity.iceologer.ambient");
 	public static final RegistryObject<SoundEvent> ENTITY_ICEOLOGER_HURT = HELPER.createSoundEvent("entity.iceologer.hurt");
 	public static final RegistryObject<SoundEvent> ENTITY_ICEOLOGER_DEATH = HELPER.createSoundEvent("entity.iceologer.death");
 	public static final RegistryObject<SoundEvent> ENTITY_ICEOLOGER_CELEBRATE = HELPER.createSoundEvent("entity.iceologer.celebrate");
 	public static final RegistryObject<SoundEvent> ENTITY_ICEOLOGER_CAST_SPELL = HELPER.createSoundEvent("entity.iceologer.cast_spell");
+
+	public static final RegistryObject<SoundEvent> ENTITY_TRICKSTER_AMBIENT = HELPER.createSoundEvent("entity.trickster.ambient");
+	public static final RegistryObject<SoundEvent> ENTITY_TRICKSTER_STEP = HELPER.createSoundEvent("entity.trickster.step");
+	public static final RegistryObject<SoundEvent> ENTITY_TRICKSTER_HURT = HELPER.createSoundEvent("entity.trickster.hurt");
+	public static final RegistryObject<SoundEvent> ENTITY_TRICKSTER_DEATH = HELPER.createSoundEvent("entity.trickster.death");
+	public static final RegistryObject<SoundEvent> ENTITY_TRICKSTER_CELEBRATE = HELPER.createSoundEvent("entity.trickster.celebrate");
+	public static final RegistryObject<SoundEvent> ENTITY_TRICKSTER_LAUGH = HELPER.createSoundEvent("entity.trickster.laugh");
+
 	public static final RegistryObject<SoundEvent> ENTITY_PLAYER_CAST_SPELL = HELPER.createSoundEvent("entity.player.cast_spell");
 	public static final RegistryObject<SoundEvent> ENTITY_CREEPER_SPORES_THROW = HELPER.createSoundEvent("entity.creeper_spores.throw");
 	public static final RegistryObject<SoundEvent> BLOCK_RUNED_GLOOMY_TILES_ACTIVATE = HELPER.createSoundEvent("block.runed_gloomy_tiles.activate");
