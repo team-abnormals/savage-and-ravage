6a4c13fd3bc96b09574d4db447bf1091af822c8f
abigailfails <thatvaliantvole@gmail.com>
2021-08-07 17:58:32 +0100

    Reverted AbstractRaiderEntityMixin change

--- src/main/java/com/minecraftabnormals/savageandravage/core/mixin/AbstractRaiderEntityMixin.java
+++ src/main/java/com/minecraftabnormals/savageandravage/core/mixin/AbstractRaiderEntityMixin.java
@@ -1,20 +1,30 @@
 package com.minecraftabnormals.savageandravage.core.mixin;
 
 import com.minecraftabnormals.savageandravage.core.SRConfig;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.monster.AbstractRaiderEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.monster.PatrollerEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * @author - SmellyModder (Luke Tonon)
  */
 @Mixin(AbstractRaiderEntity.class)
-public abstract class AbstractRaiderEntityMixin {
-	@Redirect(method = "die", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addEffect(Lnet/minecraft/potion/EffectInstance;)Z"))
-	private boolean cancelBadOmenEffect(PlayerEntity player, EffectInstance effect) {
-		return !(SRConfig.COMMON.noBadOmenOnDeath.get() || player.addEffect(effect));
+public abstract class AbstractRaiderEntityMixin extends PatrollerEntity {
+	private AbstractRaiderEntityMixin(EntityType<? extends PatrollerEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Inject(method = "die", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameRules;getBoolean(Lnet/minecraft/world/GameRules$RuleKey;)Z", shift = At.Shift.AFTER), cancellable = true)
+	private void cancelBadOmenEffect(DamageSource source, CallbackInfo info) {
+		if (SRConfig.COMMON.noBadOmenOnDeath.get()) {
+			super.die(source);
+			info.cancel();
+		}
 	}
 }
