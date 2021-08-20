f86b92691c82e4acc52d3c887618ae9e366ab4fb
abigailfails <thatvaliantvole@gmail.com>
2021-08-20 12:37:54 +0100

    Weight effect colour

--- src/main/java/com/minecraftabnormals/savageandravage/common/effect/WeightEffect.java
+++ src/main/java/com/minecraftabnormals/savageandravage/common/effect/WeightEffect.java
@@ -7,7 +7,7 @@ import net.minecraft.potion.EffectType;
 
 public class WeightEffect extends Effect {
 	public WeightEffect() {
-		super(EffectType.HARMFUL, 5926018); //TODO change colour
+		super(EffectType.HARMFUL, 0x4C5367);
 		this.addAttributeModifier(Attributes.MOVEMENT_SPEED, "7107DE5E-7CE8-4030-940E-514C1F160890", -0.15F, AttributeModifier.Operation.MULTIPLY_TOTAL);
 	}
 }
