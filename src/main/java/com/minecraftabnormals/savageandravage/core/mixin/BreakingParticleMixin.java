434a7134606a884d7b54e78fa837e8d3e2b1f6b9
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-12-07 20:58:56 -0600

    Fix all_effects advancement modifier not working

--- src/main/java/com/minecraftabnormals/savageandravage/core/mixin/BreakingParticleMixin.java
+++ src/main/java/com/minecraftabnormals/savageandravage/core/mixin/BreakingParticleMixin.java
@@ -23,6 +23,7 @@ public abstract class BreakingParticleMixin extends SpriteTexturedParticle {
 	@Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/client/world/ClientWorld;DDDLnet/minecraft/item/ItemStack;)V")
 	private void getModel(ClientWorld level, double p_i232348_2_, double p_i232348_4_, double p_i232348_6_, ItemStack stack, CallbackInfo ci) {
 		if (stack.getItem() == SRItems.CLEAVER_OF_BEHEADING.get()) {
+			// This is a crappy fix - and just removes the particles altogether - a way needs to be found to use the `cleaver_of_beheading_gui` model or fix the forge bug.
 			IBakedModel model = Minecraft.getInstance().getItemRenderer().getItemModelShaper().getItemModel(Items.AIR);
 			if (model != null)
 				this.setSprite(model.getParticleIcon());
