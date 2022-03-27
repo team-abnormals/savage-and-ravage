027d5566ea54a844d75fe45c283bbafd8a38cfa7
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-03-27 01:34:47 -0500

    Slight fixes

--- src/main/java/com/teamabnormals/savage_and_ravage/core/data/server/modifiers/SRAdvancementModifiersProvider.java
+++ src/main/java/com/teamabnormals/savage_and_ravage/core/data/server/modifiers/SRAdvancementModifiersProvider.java
@@ -1,8 +1,11 @@
 package com.teamabnormals.savage_and_ravage.core.data.server.modifiers;
 
+import com.teamabnormals.blueprint.common.advancement.modification.modifiers.EffectsChangedModifier;
 import com.teamabnormals.blueprint.common.advancement.modification.modifiers.ParentModifier;
 import com.teamabnormals.blueprint.core.util.modification.AdvancementModifierProvider;
 import com.teamabnormals.savage_and_ravage.core.SavageAndRavage;
+import com.teamabnormals.savage_and_ravage.core.registry.SRMobEffects;
+import net.minecraft.advancements.critereon.MobEffectsPredicate;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
 
@@ -16,5 +19,6 @@ public class SRAdvancementModifiersProvider extends AdvancementModifierProvider 
 	protected void registerEntries() {
 		this.registerEntry("adventure/ol_betsy", new ParentModifier(new ResourceLocation("adventure/shoot_arrow")), new ResourceLocation("adventure/ol_betsy"));
 		this.registerEntry("adventure/voluntary_exile", new ParentModifier(new ResourceLocation("adventure/trade")), new ResourceLocation("adventure/voluntary_exile"));
+		this.registerEntry("nether/all_effects", new EffectsChangedModifier("all_effects", false, MobEffectsPredicate.effects().and(SRMobEffects.WEIGHT.get())), new ResourceLocation(("nether/all_effects")));
 	}
 }
