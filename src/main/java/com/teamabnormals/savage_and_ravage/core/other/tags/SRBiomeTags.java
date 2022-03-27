98890c911b1403310616f604031d60aa63385496
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-03-27 01:45:22 -0500

    Forgot to commit

--- src/main/java/com/teamabnormals/savage_and_ravage/core/other/tags/SRBiomeTags.java
+++ src/main/java/com/teamabnormals/savage_and_ravage/core/other/tags/SRBiomeTags.java
@@ -4,7 +4,6 @@ import com.teamabnormals.blueprint.core.util.TagUtil;
 import com.teamabnormals.savage_and_ravage.core.SavageAndRavage;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.Block;
 
 public class SRBiomeTags {
 	public static final TagKey<Biome> HAS_ICEOLOGER = biomeTag("has_monster/iceologer");
--- src/main/java/com/teamabnormals/savage_and_ravage/core/registry/SRSounds.java
+++ src/main/java/com/teamabnormals/savage_and_ravage/core/registry/SRSounds.java
@@ -3,10 +3,10 @@ package com.teamabnormals.savage_and_ravage.core.registry;
 import com.teamabnormals.blueprint.core.util.registry.SoundSubRegistryHelper;
 import com.teamabnormals.savage_and_ravage.core.SavageAndRavage;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.registries.RegistryObject;
 
-@Mod.EventBusSubscriber(modid = SavageAndRavage.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = SavageAndRavage.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class SRSounds {
 	public static final SoundSubRegistryHelper HELPER = SavageAndRavage.REGISTRY_HELPER.getSoundSubHelper();
 
