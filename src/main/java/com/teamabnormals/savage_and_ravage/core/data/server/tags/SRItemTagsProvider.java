fddb86f4caac593414294ac6d6b40b9197df33a6
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-12-31 15:46:54 -0600

    just put the fries in the bag bro
    Closes #145

--- src/main/java/com/teamabnormals/savage_and_ravage/core/data/server/tags/SRItemTagsProvider.java
+++ src/main/java/com/teamabnormals/savage_and_ravage/core/data/server/tags/SRItemTagsProvider.java
@@ -11,6 +11,8 @@ import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.Tags.Items;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 import java.util.concurrent.CompletableFuture;
@@ -28,6 +30,11 @@ public class SRItemTagsProvider extends ItemTagsProvider {
 		this.copy(BlockTags.SLABS, ItemTags.SLABS);
 		this.copy(BlockTags.STAIRS, ItemTags.STAIRS);
 		this.copy(BlockTags.WALLS, ItemTags.WALLS);
+
+		this.tag(Items.ARMORS_HELMETS).add(SRItems.GRIEFER_HELMET.get(), SRItems.MASK_OF_DISHONESTY.get());
+		this.tag(Items.ARMORS_CHESTPLATES).add(SRItems.GRIEFER_CHESTPLATE.get());
+		this.tag(Items.ARMORS_LEGGINGS).add(SRItems.GRIEFER_LEGGINGS.get());
+		this.tag(Items.ARMORS_BOOTS).add(SRItems.GRIEFER_BOOTS.get());
 
 		this.tag(SRItemTags.EXPLOSION_IMMUNE).add(SRItems.CREEPER_SPORES.get(), SRItems.GRIEFER_HELMET.get(), SRItems.GRIEFER_CHESTPLATE.get(), SRItems.GRIEFER_LEGGINGS.get(), SRItems.GRIEFER_BOOTS.get(), SRBlocks.BLAST_PROOF_PLATES.get().asItem(), SRBlocks.BLAST_PROOF_STAIRS.get().asItem(), SRBlocks.BLAST_PROOF_SLAB.get().asItem());
 	}
